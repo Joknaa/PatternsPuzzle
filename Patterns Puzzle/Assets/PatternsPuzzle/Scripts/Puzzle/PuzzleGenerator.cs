b709e9e1279a74713067c0d40479e90a4ee79488
Oknaa <mohammadlaadidaoui@gmail.com>
2022-12-13 18:31:48 +0100

    Fixed some issues in tile sizes and positions during generation
    - Fixed some bugs in the tile generation
    - Fixed some bugs in the tile highlight (TileShadow) generation

---
+++ Patterns Puzzle/Assets/PatternsPuzzle/Scripts/Puzzle/PuzzleGenerator.cs
@@ -0,0 +1,71 @@
+﻿using OknaaEXTENSIONS;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PuzzleSystem {
+    public class PuzzleGenerator {
+        public static PuzzleGenerator Instance => instance ??= new PuzzleGenerator();
+        private static PuzzleGenerator instance;
+        
+        private Puzzle _puzzle;
+        private Texture2D _originalImage;
+        private Texture2D _originalShadowImage;
+        private Vector2Int _tileCount;
+        private Tile _tilePrefab;
+        private Transform _tilesContainer;
+        private bool _isInit;
+
+
+        public void GeneratePuzzle(Puzzle puzzle) {
+            if (!_isInit) Init(puzzle);
+
+            GeneratePuzzleTiles();
+        }
+        
+        public void Clear() => _isInit = false;
+        
+        private void GeneratePuzzleTiles() {
+            _originalImage.SplitTextureIntoSprites(_tileCount, out var sprites);
+
+            Vector2Int coordinates;
+            for (var index = 0; index < sprites.Count; index++) {
+                _puzzle.tiles.Add(InstantiateTilePrefab(index));
+            }
+
+            Tile InstantiateTilePrefab(int index) {
+                coordinates = _puzzle.TileIndex2Coordinates(index);
+                var tileInstance = Object.Instantiate(_tilePrefab, _tilesContainer);
+                SetTileDimensionsRelativeToParentInCanvas(tileInstance, _tilesContainer, coordinates, _tileCount);
+                tileInstance.Init(_puzzle, coordinates, sprites[index]);
+                return tileInstance;
+            }
+        }
+
+        private void Init(Puzzle puzzle) {
+            _puzzle = puzzle;
+            _originalImage = _puzzle._inputImage;
+            _tileCount = _puzzle._tileCount;
+            _tilePrefab = _puzzle.tilePrefab;
+            _tilesContainer = _puzzle.tilesContainer;
+            _isInit = true;
+        }
+        
+        public static void SetTileDimensionsRelativeToParentInCanvas(Object tile, Object parentInCanvas, Vector2Int tileCoordinates, Vector2Int tileCount) {
+            var i = tileCoordinates.x;
+            var j = tileCoordinates.y;
+            
+            var parentRect = parentInCanvas.GetComponent<RectTransform>().rect;
+            var parentDimensions = new Vector2(parentRect.width, parentRect.height);
+            var tileDimensions = new Vector2(parentDimensions.x / tileCount.x, parentDimensions.y / tileCount.y);
+            Debug.Log("tileDimensions: " + tileDimensions);
+            
+            var tileRectTransform = tile.GetComponent<RectTransform>();
+            tileRectTransform.localScale = Vector3.one; 
+            tileRectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, tileDimensions.x);
+            tileRectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, tileDimensions.y);
+            tileRectTransform.localPosition = new Vector3(i * tileDimensions.x, j * tileDimensions.y, 0);
+
+        }
+    }
+}
---
+++ Patterns Puzzle/Assets/PatternsPuzzle/Scripts/Puzzle/Tiles/Tile.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using GameControllers;
+using OknaaEXTENSIONS;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+namespace PuzzleSystem {
+    public class Tile : MonoBehaviour {
+        public Image spriteRenderer;
+        public TileShadow shadow;
+
+        public int X;
+        public int Y;
+        public bool isTaken;
+        public TileGroup tileGroupParent;
+
+        private readonly List<Tile> neighbouringTiles = new List<Tile>();
+        private Puzzle _puzzle;
+        private float _combiningChance;
+        private int _numberOfTilesToCombineWith = 0;
+
+        private bool _isMatched = false;
+        private RectTransform _rectTransform;
+        
+        public void Init(Puzzle puzzle, Vector2Int coord, Sprite sprite) {
+            _puzzle = puzzle;
+            X = coord.x;
+            Y = coord.y;
+            _combiningChance = _puzzle.combiningChance;
+            _numberOfTilesToCombineWith = _puzzle.numberOfTilesToCombine;
+            spriteRenderer.sprite = sprite;
+            gameObject.name = $"Tile {X} {Y}";
+            
+            shadow.Init(_puzzle, coord, this);
+        }
+        
+
+        public void SetUpNeighbours() {
+            if (_puzzle.GetTileByIndex(X, Y + 1, out var tileAbove)) neighbouringTiles.Add(tileAbove);
+            if (_puzzle.GetTileByIndex(X, Y - 1, out var tileBelow)) neighbouringTiles.Add(tileBelow);
+            if (_puzzle.GetTileByIndex(X - 1, Y, out var tileLeft)) neighbouringTiles.Add(tileLeft);
+            if (_puzzle.GetTileByIndex(X + 1, Y, out var tileRight)) neighbouringTiles.Add(tileRight);
+        }
+
+        public void CombineTileWithRandomNeighbors() {
+            if (isTaken) return;
+            
+            List<Tile> tilesToCombineWith = new List<Tile>() { this };
+            if (CannotCombine()) {
+                CreateTileGroup(tilesToCombineWith);
+                return;
+            }
+
+            Tile randomTile;
+            while (_numberOfTilesToCombineWith > 0) {
+                if (AllNeighbouringTilesAreTaken) break;
+
+                randomTile = neighbouringTiles.Random();
+                if (randomTile.isTaken) continue;
+
+                tilesToCombineWith.Add(randomTile);
+                _numberOfTilesToCombineWith--;
+            }
+            CreateTileGroup(tilesToCombineWith);
+        }
+        
+        private void CreateTileGroup(List<Tile> tilesToCombine) {
+            var tileGroup = Instantiate(_puzzle.tileGroupPrefab, _puzzle.tilesContainer);
+            tileGroup.Init(tilesToCombine, _puzzle);
+            
+            var tileGroupRectTransform = tileGroup.GetComponent<RectTransform>();
+            tileGroupRectTransform.localPosition = GetAveragePosition();
+
+            foreach (var tile in tilesToCombine) {
+               tile.HandleTileGrouping(tileGroup);
+            }
+            
+            tileGroup.transform.SetParent(_puzzle.tilesContainer);
+            _puzzle.tileGroups.Add(tileGroup);
+
+            Vector3 GetAveragePosition() {
+                var averagePosition = Vector3.zero;
+                foreach (var tile in tilesToCombine) {
+                    averagePosition += tile.GetComponent<RectTransform>().localPosition;
+                }
+
+                return averagePosition / tilesToCombine.Count;
+            }
+        }
+
+        private void HandleTileGrouping(TileGroup tileGroup) {
+            isTaken = true;
+            tileGroupParent = tileGroup;
+            transform.SetParent(tileGroup.transform);
+            transform.localScale = Vector3.one;
+            _numberOfTilesToCombineWith--;
+        }
+        
+        private bool AllNeighbouringTilesAreTaken => neighbouringTiles.TrueForAll(tile => tile.isTaken);
+        private bool CannotCombine() => !(Random.Range(0f, 1f) < _combiningChance);
+    }
+}
